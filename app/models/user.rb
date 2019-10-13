169c089a2922bf9b09a6a2ad755071c4a590c658
acollie <12976663+Acollie@users.noreply.github.com>
2019-10-13 22:22:59 +0100

    Made changes to how rake task is run, after I sought advice.

--- app/models/user.rb
+++ app/models/user.rb
@@ -146,7 +146,7 @@ class User < ApplicationRecord
     #TODO make decision on how this should be run
     require 'rake'
     Rails.application.load_tasks
-    Rake::Task['data_export:Add'].invoke
+    # Rake::Task["data_export:Add[#{self.user.id}]"].invoke
 
   end
   def google_access_token_expired?
