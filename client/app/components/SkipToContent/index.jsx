e247a61856c03bfa0ef62b792af2ae2bd14a71ae
Sofiya Tepikin <sofiya.tepikin@gmail.com>
2020-04-10 19:55:29 +0200

    Add skip to content component

---
+++ client/app/components/SkipToContent/index.jsx
@@ -0,0 +1,9 @@
+// @flow
+import React from 'react';
+import css from './SkipToContent.scss';
+
+export const SkipToContent = () => {
+  return (
+    <p className={css.test}>Skip to content</p>
+  );
+};
